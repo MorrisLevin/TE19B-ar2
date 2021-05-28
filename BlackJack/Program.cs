2b8ee1435a7ef71a78140bb08605c4b1ac3b0437
Morris Levin <morris.levin@elev.ga.ntig.se>
2021-05-28 10:37:22 +0200

    BlackJack

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -5,35 +5,130 @@ namespace BlackJack
     class Program
     {
         static Random slump = new Random();
+
         static void Main(string[] args)
         {
+            //Skapa en array med klädda kort
+            string[] kläddkort = new string[] { "Queen", "King", "Jack" };
 
-            string svar = "";
+            // Generera ett slumpmässigt index som är mindre än storleken på arrayen.
+            int randomkläddkort = slump.Next(kläddkort.Length);
 
-            while (svar != "n")
+
+            //Skapa en array fylld med kortfärger
+            string[] färg = new string[] { "of spades", "of cloves", "of clubs", "of hearts" };
+
+            // Generera ett slumpmässigt index som är mindre än storleken på arrayen.
+            int randomfärg = slump.Next(färg.Length);
+
+
+            //Skapa en int som samlar båda spelarnas värde
+            int[] total = { 0, 0 };
+
+            //Förklara reglerna av BlackJack
+            Console.WriteLine("Welcome to the game BlackJack.\nThe rules are:\n1. You will draw multiple random cards.\n2. With these cards you have to get them to as close to the number 21.\n3. If you go over the number 21, you lose. Player 1 starts with a random card.");
+
+
+            //While loop som ser till att total[i] värde inte blir högre än 21
+            for (int i = 0; i < total.Length; i++)
             {
-                Console.WriteLine("Welcome to the game BlackJack. Would you like to begin? y = yes n = no");
-                svar = Console.ReadLine();
-
-                if (svar == "y")
+                while (total[i] <= 21) // resten
                 {
-                    Console.WriteLine("OK, then you can start!");
+
+                    //Int för att dra ett random kort med ett random värde
+                    int kort = SlumpaKort();
+
+
+                    //Total[i] samlar värderna från int kort
+                    total[i] += kort;
+
+
+                    //Om total[i] värde blir över 21, så vinner spelare 2
+                    if (total[i] > 21)
+                    {
+                        Console.WriteLine($"Your value was {total[i]} That value is over 21, You lost, Player {i + 0} wins.");
+                        break;
+                    }
+
+
+
+                    //Bestämmer hur äss fungerar
+                    if (kort == 1)
+                    {
+                        Console.WriteLine("You got an ace. Choose between 1 and 11. 1 = a 11 = b");
+                        string ace = Console.ReadLine();
+
+                        if (ace == "a")
+                        {
+                            kort = 1;
+                        }
+                        else if (ace == "b")
+                        {
+                            total[i] = total[i] + 10;
+                        }
+                    }
+
+                    //Förklarar vilket kort man får/vilket värde ens total har. Frågar också om man vill dra ett till kort. Ser också till så att om man får nummer 10 så kan man få ett klädd kort.
+                    if (kort != 10)
+                    {
+
+
+                        Console.WriteLine($"The card you got was the {kort} {färg[randomfärg]}. Your value is now {total[i]}. Do you want another card? y = yes n = no ");
+                        string svar = Console.ReadLine();
+
+                        if (svar == "n")
+                        {
+                            Console.WriteLine($"Ok, now player {i + 2}, your turn.");
+                            break;
+                        }
+                    }
+
+
+                    else
+                    {
+                        kort = 10;
+                        Console.WriteLine($"The card you got was the {kläddkort[randomkläddkort]} {färg[randomfärg]}. Your value is now {total[i]}. Do you want another card? y = yes n = no ");
+                        string svar = Console.ReadLine();
+                        if (svar == "n")
+                        {
+                            Console.WriteLine($"Ok, now player {i + 2}, your turn.");
+
+
+
+                            break;
+                        }
+                    }
                 }
-                else
-                {
-                    break;
-                }
-static void 
-                Console.WriteLine($"First, we have player 1. Plaayer 1, your first card is: {SlumpaKort()}";
-
-             static int SlumpaKort()
-        {
-            int rad = slump.Next(1, 10);
-
-            return rad;
-        }
-
             }
+
+
+            if (total[0] == total[1])
+            {
+                Console.WriteLine("It's a draw!");
+            }
+            else if (total[0] > total[1] && total[0] <= 21)
+            {
+                Console.WriteLine("Player 1 wins!");
+            }
+            else
+            {
+                Console.WriteLine("Player 2 wins!");
+            }
+
+
         }
+        //Metod som ger ut värdet till korten, mellan 1 och 10
+        static int SlumpaKort()
+        {
+            int nummer = slump.Next(1, 11);
+
+            return nummer;
+        }
+
+
+
+
     }
 }
+
+
